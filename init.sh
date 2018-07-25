8017ffd351e58bec2dc3ba3c08a434e4433b7f27
Keith Manville <kmanville@mitre.org>
2018-07-25 08:51:23 -0400

    added init for x and i3

--- init.sh
+++ init.sh
@@ -8,5 +8,10 @@ ln -s `pwd`/tmux.conf ~/.tmux.conf
 ln -s `pwd`/vim/ ~/.vim
 ln -s `pwd`/vim/vimrc ~/.vimrc
 ln -s `pwd`/zshrc ~/.zshrc
+ln -s `pwd`/Xresources ~/.Xresources
+ln -s `pwd`/Xmodmap ~/.Xmodmap
+ln -s `pwd`/xinitrc ~/.xinitrc
+ln -s `pwd`/i3status.conf ~/.i3status.conf
+mkdir ~/.i3 && ln -s `pwd`/i3config ~/.i3/config
 mkdir ~/.vim/undo
 vim +BundleInstall +qall
